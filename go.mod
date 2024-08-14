f10436423e84d0dc183912121714bcd02ddfb47c
Kirito <2493381254@qq.com>
2024-08-15 00:07:28 +0800

    [init]:限流系统初始化

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kiritoabc/go-limiter
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// 计数器算法
+// 滑动窗口算法
+// 漏桶算法
+// 令牌桶算法
+// 单机限流
+// 分布式限流
+
+func main() {
+	fmt.Println("hello go limiter")
+}
